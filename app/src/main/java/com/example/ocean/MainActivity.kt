9092635e68acb26d4bded4bb38df727b8d6fd314
student <student@admins-iMac.local>
2022-12-23 13:15:29 +0530

    Added bottom navigation views

--- app/src/main/java/com/example/ocean/MainActivity.kt
+++ app/src/main/java/com/example/ocean/MainActivity.kt
@@ -4,8 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+//    ActivityMainBinding binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        binding = ActivityMainBinding.inflate(layoutInflater());
+//        setContentView(binding.getRoot());
+
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//
+//            switch(item.getItemID()){
+//                case R.id.watchlist:
+//                        break;
+//
+//                case R.id.orders:
+//                        break;
+//
+//                case R.id.portfolio:
+//                        break;
+//
+//                case R.id.funds:
+//                        break;
+//            }
+//
+//            return true;
+//        })
+
     }
 }
