064b0ba8efb3fae80f374700a88748c2dc801ebb
student <student@admins-iMac.local>
2022-12-23 16:02:29 +0530

    Added fragments

--- app/src/main/java/com/example/ocean/MainActivity.kt
+++ app/src/main/java/com/example/ocean/MainActivity.kt
@@ -2,14 +2,40 @@ package com.example.ocean
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ocean.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
 //    ActivityMainBinding binding
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(watchlistFragment())
+
+        // till now we have implemented viewbinding
+
+//        setContentView(R.layout.activity_main)   previously line was this.
+
+//        Now applying on Click event Listener on our menu items.
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.watchlist -> replaceFragment(watchlistFragment())
+                R.id.orders -> replaceFragment(ordersFragment())
+                R.id.portfolio -> replaceFragment(portfolioFragment())
+                R.id.funds -> replaceFragment(fundsFragment())
+
+                else -> {}
+
+            }
+            true
+        }
 
 //        binding = ActivityMainBinding.inflate(layoutInflater());
 //        setContentView(binding.getRoot());
@@ -33,5 +59,14 @@ class MainActivity : AppCompatActivity() {
 //            return true;
 //        })
 
+    }
+    // now we are creating method to replace fram layout
+    // replacing fram layout with fragment. We used fragmentManager to transact frameLayout with the fragment that we will get as an argument
+    // in replaceFragment() functionl.
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
